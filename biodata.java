cfe9072cca8b1e9e64c927a5544f8132f54f864e
Alex2sf <97343955+Alex2sf@users.noreply.github.com>
2022-04-04 21:58:25 +0700

    Add files via upload

---
+++ biodata.java
@@ -0,0 +1,37 @@
+package java;
+
+public class biodata {
+    String namaLengkap = "Alex Zaro Daeli";
+    String tempatLahir = "Tangerang";
+    String tangggalLahir = "05-05-2003";
+    String hobi = "Desain";
+    String zodiak = "Taurus";
+    String makananFavorit = "nasi goreng";
+    String citaCita = "Dokter";
+
+    public void getFullName() {
+        System.out.println("Nama Lengkap = " + namaLengkap );
+    } 
+
+    public void getBirthPlace() {
+        System.out.println("Tempat Lahir = " + tempatLahir );
+    } 
+
+    public void getHobby() {
+        System.out.println("Hobi = " + hobi );
+    } 
+
+    public void getZodiak() {
+        System.out.println("Zodiak  = " + zodiak );
+    } 
+
+    public void getFavoriteFood() {
+        System.out.println("Makanan Favorite = " + makananFavorit );
+    } 
+
+
+    public void getDream() {
+        System.out.println("Cita Cita = " + citaCita );
+    } 
+    
+}
---
+++ main.java
@@ -0,0 +1,13 @@
+package java;
+import biodata.*;
+
+public class main {
+    Biodata bio = new Biodata();
+    bio.getFullName();
+    bio.getBirthPlace();
+    bio.getZodiak();
+    bio.getHobby(); 
+    bio.getFavoriteFood();
+    bio.getDream();
+    }
+
